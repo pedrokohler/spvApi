9026f9010a4d58106e9d14f43aa4b5d5fbbdbfeb
Pedro Köhler <pedrokohlerbh@gmail.com>
2019-09-10 01:41:41 -0300

    Fix linter config

--- src/services/number.ts
+++ src/services/number.ts
@@ -1,11 +1,9 @@
-export default class NumberAbs { //can't name it Number because of JS's reserved names.
+export default class NumberAbs { // can't name it Number because of JS's reserved names.
+  public sum(a: number, b: number): number {
+    return a + b;
+  }
 
-    public sum(a: number, b: number): number{
-        return a+b;
-    }
-
-    public divide(a: number, b: number): number{
-        return a/b;
-    }
-
+  public divide(a: number, b: number): number {
+    return a / b;
+  }
 }
